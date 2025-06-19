1795e295587028466f876aac61a440ae55b7096c
NakyRs <naufalajaa6@gmail.com>
2025-06-19 13:40:41 +0700

    update

--- App.java
+++ App.java
@@ -4,14 +4,3 @@ public class App {
         matchCards.showHomeFrame();
     }
 }
-// public class App {
-//     public static void main(String[] args) {
-//         javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//             @Override
-//             public void run() {
-//                 MatchCards game = new MatchCards();
-//                 game.showHomeFrame(); // Mulai dari menu home
-//             }
-//         });
-//     }
-// }
