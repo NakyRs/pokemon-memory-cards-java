bffa8ae16800e1d49ed83df2c2c4fe08c9be54e4
NakyRs <naufalajaa6@gmail.com>
2025-06-21 20:24:29 +0700

    Update

--- MatchCards.java
+++ MatchCards.java
@@ -18,33 +18,7 @@ public class MatchCards {
         }
     }
 
-    String[] pokeList = { //track cardNames
-        "darkness",
-        "double",
-        "fairy",
-        "fighting",
-        "fire",
-        "grass",
-        "lightning",
-        "metal",
-        "psychic",
-        "water"
-    };
-    // If you need the second set, declare it separately:
-    String[] characterCardList = {
-        "char_brook_b",
-        "char_chopper_b",
-        "char_franky_b",
-        "char_luffy_b",
-        "char_nami_b",
-        "char_robin_b",
-        "char_sanji_b",
-        "char_zoro_b",
-        "char_usopp_b",
-        "char_jinbe_b"
-    };
-
-    String[] cardList = pokeList.clone(); // default
+    String[] cardList = tema.pokeList.clone(); // default
 
     int rows = 4;
     int columns = 5;
@@ -54,10 +28,10 @@ public class MatchCards {
     ArrayList<Card> cardSet; //create a deck of cards with cardNames and cardImageIcons
     ImageIcon cardBackImageIcon;
 
-    int boardWidth = columns * cardWidth; //5*128 = 640px
-    int boardHeight = rows * cardHeight; //4*90 = 360px
+    int boardWidth = columns * cardWidth; //5*90 = 450px
+    int boardHeight = rows * cardHeight; //4*128 = 512px
 
-    JFrame frame = new JFrame("Pokemon Match Cards");
+    JFrame frame = new JFrame("Match Cards");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
     JPanel boardPanel = new JPanel();
@@ -77,8 +51,9 @@ public class MatchCards {
         COUNT_UP, COUNT_DOWN, FREE
     }
 
+    String selectedTheme= "Pokemon"; // default theme
     GameMode selectedMode = GameMode.COUNT_UP;
-    int countdownSeconds = 5; // default seconds for COUNT_DOWN mode
+    int countdownSeconds = 5; // default
 
     
     void play() {
@@ -86,14 +61,12 @@ public class MatchCards {
         boardPanel = new JPanel();
         restartGamePanel = new JPanel();
 
-        // showHomeFrame();
-        // showGameModeDialog();
         setupCards();
         shuffleCards();
 
         frame.setLayout(new BorderLayout());
         frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
+        // frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -126,11 +99,10 @@ public class MatchCards {
                         JOptionPane.showMessageDialog(frame, "You Lose!!", "Game Over", JOptionPane.INFORMATION_MESSAGE);
                         gameReady = false;
                         restartButton.setEnabled(true);
-                        // Reset frame & back to play()
+
                         restohome();
                     }
                 });
-            // timer.start();
         } else if (selectedMode == GameMode.COUNT_UP) {
             seconds = 0;
             timerLabel.setText("Time: " + seconds);
@@ -138,8 +110,7 @@ public class MatchCards {
                     seconds++;
                     timerLabel.setText("Time: " + seconds);
                 });
-            // timer.start();
-        } else { // FREE MODE
+        } else { 
             timerLabel.setText("Free Mode");
             timer = null;
         }
@@ -177,10 +148,7 @@ public class MatchCards {
                                 hideCardTimer.start();
                             }
                             else {
-                                // caard matched
-                                // Disable the matched cards
-                                // card1Selected.setEnabled(false);
-                                // card2Selected.setEnabled(false);
+                                // card matched
                                 card1Selected = null;
                                 card2Selected = null;
                                 
@@ -198,7 +166,7 @@ public class MatchCards {
         ///restart game button
         restartButton.setFont(new Font("Arial", Font.PLAIN, 16));
         restartButton.setText("Restart Game");
-        restartButton.setPreferredSize(new Dimension(boardWidth-80 , 30));
+        restartButton.setPreferredSize(null);
         restartButton.setFocusable(false);
         restartButton.setEnabled(false);
         restartButton.addActionListener(e -> {
@@ -221,24 +189,24 @@ public class MatchCards {
                 if (selectedMode == GameMode.COUNT_DOWN) {
                     seconds = countdownSeconds;
                     timerLabel.setText("Time: " + seconds);
-                    // timer.start();
+                    
                 } else if (selectedMode == GameMode.COUNT_UP) {
                     seconds = 0;
                     timerLabel.setText("Time: " + seconds);
-                    // timer.start();
+                    
                 } else {
                     timerLabel.setText("Free Mode");
                 }
             });
-        // Panel tombol restart dan home
-        restartGamePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 10, 0));
-        restartGamePanel.add(restartButton);
+        // add restart button
+        restartGamePanel = new JPanel(new BorderLayout( 10, 0));
+        restartGamePanel.add(restartButton, BorderLayout.CENTER);
 
-        // Tambahkan home button kecil
+        // Home button
         JButton homeButton = new JButton("Home");
         homeButton.setFont(new Font("Arial", Font.PLAIN, 12));
         homeButton.setPreferredSize(new Dimension(80, 28));
-        restartGamePanel.add(homeButton);
+        restartGamePanel.add(homeButton, BorderLayout.EAST);
 
         frame.add(restartGamePanel, BorderLayout.SOUTH);
         // Action home button
@@ -247,6 +215,7 @@ public class MatchCards {
         });
 
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         
         //start game
@@ -257,12 +226,25 @@ public class MatchCards {
 
     }
 
-
     void setupCards() {
         cardSet = new ArrayList<Card>();
+        // Default source
+        String source= "./img/poke/"; 
+        String backcard= "./img/back"; 
+        if (selectedTheme.equals("Pokemon")) {
+            source= "./img/poke/"; 
+            backcard= "./img/poke/back"; 
+        } else if (selectedTheme.equals("One Piece")) {
+            source= "./img/op/";
+            backcard= "./img/op/back";
+        } else if (selectedTheme.equals("Tarot") || selectedTheme.equals("Tarot2 (Hard)")) {
+            source= "./img/tarot/";
+            backcard= "./img/tarot/back2"; 
+        }
         for (String cardName : cardList) {
+            // System.out.println(source + cardName + ".jpg");
             //load each card image
-            Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")).getImage();
+            Image cardImg = new ImageIcon(getClass().getResource(source + cardName + ".jpg")).getImage();
             ImageIcon cardImageIcon = new ImageIcon(cardImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
 
             //create card object and add to cardSet
@@ -272,7 +254,7 @@ public class MatchCards {
         cardSet.addAll(cardSet);
 
         //load the back card image
-        Image cardBackImg = new ImageIcon(getClass().getResource("./img/back.jpg")).getImage();
+        Image cardBackImg = new ImageIcon(getClass().getResource(backcard + ".jpg")).getImage();
         cardBackImageIcon = new ImageIcon(cardBackImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
     }
 
@@ -320,18 +302,16 @@ public class MatchCards {
             restartButton.setEnabled(true);
             if (timer != null) timer.stop();
             if (selectedMode == GameMode.COUNT_UP) {
-                JOptionPane.showMessageDialog(frame, "Congrats! You win!\nFinis Time: " + seconds + " Seconds", "Winning!!", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(frame, "Congrats! You win!\nFinish Time: " + seconds + " Seconds", "Winning!!", JOptionPane.INFORMATION_MESSAGE);
             } else {
                 JOptionPane.showMessageDialog(frame, "Congrats! You win!", "Winning!!", JOptionPane.INFORMATION_MESSAGE);
             }
-            // Reset frame and back to play()
+            // Reset frame
             restohome();
         }
     }
 
     void restohome(){
-        // frame.getContentPane().removeAll();
-        // frame.repaint();
         frame.dispose(); // close the current frame
         errorCount = 0;
         textLabel.setText("Errors: " + Integer.toString(errorCount));
@@ -343,47 +323,41 @@ public class MatchCards {
     }
 
     void showHomeFrame() {
-        JFrame homeFrame = new JFrame("Pokemon Memory Cards - Home");
+        JFrame homeFrame = new JFrame("Memory Cards - Home");
         homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        homeFrame.setSize(400, 300);
-        homeFrame.setLocationRelativeTo(null);
-        homeFrame.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        // Pilih Mode
+        homeFrame.setSize(320, 210); 
+        homeFrame.setLocationRelativeTo(null); 
+        homeFrame.setLayout(null);
+
+        // Mode
         JLabel modeLabel = new JLabel("Pilih Mode:");
+        modeLabel.setBounds(20, 20, 100, 25); 
         String[] modeOptions = {"StopWatch", "Time Attack", "Free Mode"};
         JComboBox<String> modeCombo = new JComboBox<>(modeOptions);
+        modeCombo.setBounds(130, 20, 150, 25); 
 
-        // Pilih Tema
+        // Theme
         JLabel themeLabel = new JLabel("Pilih Tema:");
-        String[] themeOptions = {"Default", "Pokemon", "One Piece"};
+        themeLabel.setBounds(20, 60, 100, 25); 
+        String[] themeOptions = {"Pokemon", "One Piece", "Tarot", "Tarot2 (Hard)"};
         JComboBox<String> themeCombo = new JComboBox<>(themeOptions);
+        themeCombo.setBounds(130, 60, 150, 25);
 
-        // Tombol Play
         JButton playButton = new JButton("Play");
+        playButton.setBounds(20, 110, 260, 40);
 
-        // Layout
-        gbc.gridx = 0; gbc.gridy = 0;
-        homeFrame.add(modeLabel, gbc);
-        gbc.gridx = 1;
-        homeFrame.add(modeCombo, gbc);
-
-        gbc.gridx = 0; gbc.gridy = 1;
-        homeFrame.add(themeLabel, gbc);
-        gbc.gridx = 1;
-        homeFrame.add(themeCombo, gbc);
-
-        gbc.gridx = 0; gbc.gridy = 2; gbc.gridwidth = 2;
-        homeFrame.add(playButton, gbc);
+        // add components to the frame
+        homeFrame.add(modeLabel);
+        homeFrame.add(modeCombo);
+        homeFrame.add(themeLabel);
+        homeFrame.add(themeCombo);
+        homeFrame.add(playButton);
 
         // Action Play
         playButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Set mode sesuai pilihan
                 int modeIdx = modeCombo.getSelectedIndex();
                 if (modeIdx == 1) {
                     selectedMode = GameMode.COUNT_DOWN;
@@ -392,23 +366,27 @@ public class MatchCards {
                 } else {
                     selectedMode = GameMode.COUNT_UP;
                 }
-                // Set tema sesuai pilihan (implementasi tema sesuai kebutuhan)
-                String selectedTheme = (String) themeCombo.getSelectedItem();
+
+                selectedTheme = (String) themeCombo.getSelectedItem();
                 if (selectedTheme.equals("Pokemon")) {
-                    cardList = pokeList.clone(); // Use Pokemon cards
+                    cardList = tema.pokeList.clone(); // Use Pokemon cards
                 } else if (selectedTheme.equals("One Piece")) {
-                    cardList = characterCardList.clone(); // Use One Piece cards
+                    cardList = tema.characterCardList.clone(); // Use One Piece cards
+                } else if (selectedTheme.equals("Tarot")) {
+                    cardList = tema.tarotCardList.clone(); // Use Tarot cards
+                } else if (selectedTheme.equals("Tarot2 (Hard)")) {
+                    cardList = tema.tarotCardList2.clone(); // Use Tarot2 cards
                 } else {
-                    cardList = characterCardList.clone(); // Default to character cards
+                    cardList = tema.pokeList.clone(); // Default to poke cards
                 }
-                
-                homeFrame.dispose(); // Tutup frame home
-                play(); // Mulai game
+
+                homeFrame.dispose(); 
+                play(); 
             }
         });
-
         homeFrame.setVisible(true);
     }
+
 }
 
-// TODO: perbanyak mode dan tema, tambahkan home button di game/play()
+// TODO: perbanyak mode dan tema
---
+++ tema.java
@@ -0,0 +1,64 @@
+public class tema {
+    public static String[] pokeList = { //track cardNames
+        "darkness",
+        "double",
+        "fairy",
+        "fighting",
+        "fire",
+        "grass",
+        "lightning",
+        "metal",
+        "psychic",
+        "water"
+    };
+
+    public static String[] characterCardList = {
+        "char_brook_b",
+        "char_chopper_b",
+        "char_franky_b",
+        "char_luffy_b",
+        "char_nami_b",
+        "char_robin_b",
+        "char_sanji_b",
+        "char_zoro_b",
+        "char_usopp_b",
+        "char_jinbe_b"
+    };
+
+    public static String[] tarotCardList = {
+        "fool",
+        "magician",
+        "priestess",
+        "empress",
+        "emperor",
+        "hierophant1",
+        "wheel-of-fortune",
+        "chariot",
+        "strength",
+        "hermit"
+    };
+    public static String[] tarotCardList2 = {
+    "fool",
+    "magician",
+    "priestess",
+    "empress",
+    "emperor",
+    "hierophant1",
+    "lovers",
+    "chariot",
+    "strength",
+    "hermit",
+    "wheel-of-fortune",
+    "justice",
+    "hanged-man",
+    "death",
+    "temperance",
+    "devil",
+    "tower",
+    "star",
+    "moon",
+    "sun",
+    "judgement",
+    "world"
+    };
+}
